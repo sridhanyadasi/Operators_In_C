6433c59d087b45de5869ea1626197f6a1858603a
Sri Dhanya <sridhanyadasi@gmail.com>
2026-02-23 12:59:24 +0530

    Update discntgrtrthan.c

--- discntgrtrthan.c
+++ discntgrtrthan.c
@@ -2,20 +2,31 @@
  Prompt the user to enter the original price and check if it's greater than $100 using the greater-than operator (>).
  Apply a 20% discount if the condition is met and display the final price. */
 
+ 
  #include<stdio.h>
- void main()
- {
-     int originalPrice;
 
-     printf("enter the original price of an item :");
-     scanf("%d",&originalPrice);
+int main()
+{
+    float originalPrice, discount, finalPrice;
 
-     if (originalPrice>100)
-     {
-         printf(" There is a 20 percent discount on this item");
-     }
-     else
-     {
-         printf(" There is no discount on this item");
-     }
- }
+    printf("Enter the original price of an item: ");
+    scanf("%f", &originalPrice);
+
+    if(originalPrice > 100)
+    {
+        discount = originalPrice * 0.20;
+        finalPrice = originalPrice - discount;
+
+        printf("20%% discount applied!\n");
+        printf("Discount Amount: %.2f\n", discount);
+        printf("Final Price: %.2f\n", finalPrice);
+    }
+    else
+    {
+        printf("No discount applied.\n");
+        printf("Final Price: %.2f\n", originalPrice);
+    }
+
+    return 0;
+}
+
